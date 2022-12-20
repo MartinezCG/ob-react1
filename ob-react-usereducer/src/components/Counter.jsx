1e138c82e6b2b0945343b66e8cc984dc7a4ee006
Carlos Martinez <martinez.acarlosg@gmail.com>
2022-12-20 15:11:06 -0400

    Sesion 25 - Gestionando el estado con Hooks

---
+++ ob-react-usereducer/src/components/Counter.jsx
@@ -0,0 +1,92 @@
+import React, { useReducer, useContext } from 'react';
+
+// Actions
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const myContext = React.createContext(null)
+
+const Points = () => {
+
+    const state = useContext(myContext)
+
+    return (
+        <p>Points: { state.count }</p>
+    )
+
+}
+
+const Counter = () => {
+
+    // Initial State for Reducer
+    const initialState = {
+        count: 0
+    }
+
+    // Reducer to change State
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case INCREMENT:
+                return {
+                    count: state.count + action.payload.quantity
+                }
+            case DECREMENT:
+                return {
+                    count: state.count - action.payload.quantity
+                }
+            case RESET:
+                return {
+                    count: 0
+                }
+            default:
+                return state;
+        }
+    }
+
+    // Asign useReducer to State, reducer and dispatch actions
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <myContext.Provider value = {state}>
+            <div>
+                {/* <p>Points: { state.count }</p> */}
+                <Points/>
+                <button
+                    onClick={
+                        () => dispatch({
+                            type: INCREMENT,
+                            payload: {
+                                quantity: 1
+                                }
+                            })
+                    }
+                >
+                    INCREMENT
+                </button>
+                <button
+                    onClick={
+                        () => dispatch({
+                            type: DECREMENT,
+                            payload: {
+                                quantity: 1
+                                }
+                            })
+                    }
+                >
+                    DECREMENT
+                </button>
+                <button
+                    onClick={
+                        () => dispatch({type: RESET})
+                    }
+                >
+                    RESET COUNTER
+                </button>
+            </div>
+        </myContext.Provider>
+    );
+}
+
+export default Counter;
